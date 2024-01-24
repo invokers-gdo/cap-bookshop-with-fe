f79079dbd6e80d24367e357872cc2edf4ef96207
Gregory Doukas <gdo@trifork.com>
2024-01-24 17:03:09 +0100

    Update to CAP CDS 7.5

--- srv/server.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('@sap/cds-odata-v2-adapter-proxy');
-const cds = require('@sap/cds');
-const cors = require('cors');
-cds.on('bootstrap', (app) => {
-    app.use(cors());
-    app.use(proxy());
-})
-module.exports = cds.server
